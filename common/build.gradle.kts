0de35380a935a9e111bb09d8ed711e69649140a0
AbdelrahmanEsam <abdofahd20000@gmail.com>
2023-01-03 07:17:39 +0200

    convention plugins implementation

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,43 +1,11 @@
 plugins {
-    id("easy.android.library")
+    id("easy.android.feature")
+    id("easy.android.library.compose")
 }
 
 android {
     namespace = "com.apptikar.common"
-    compileSdk = 32
 
-    defaultConfig {
-        minSdk = 24
-        targetSdk = 32
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion  = "1.3.2"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
@@ -45,16 +13,8 @@ dependencies {
     implementation(libs.androidx.core.kotlin)
     implementation (libs.androidx.compose.ui)
     implementation (libs.androidx.compose.material)
-    implementation (libs.androidx.compose.tooling)
-    implementation (libs.androidx.lifecycle.runtime)
-    implementation (libs.androidx.activity.compose)
 
     testImplementation (libs.junit)
-    androidTestImplementation (libs.androidx.test.ext)
-    androidTestImplementation (libs.androidx.test.espresso)
-    androidTestImplementation (libs.androidx.compose.ui.test)
-    debugImplementation (libs.androidx.compose.tooling)
-    debugImplementation (libs.androidx.compose.ui.test.manifest)
 
     //dataStore
     implementation (libs.androidx.datastore.pref)
